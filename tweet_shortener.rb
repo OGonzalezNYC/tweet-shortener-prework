00284f83ac8539cf7cd1d6b5723140d01cd835cf
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-16 04:57:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,13 +4,14 @@ end
 
 def word_substituter(tweet)
   tweet_words = tweet.split(" ")
-  tweet_words.collect do |tword|
+  new_tweet_words = tweet_words.collect do |tword|
     if dictionary.keys.include?(tword.downcase)
        tweet_words[tweet_words.index(tword)] = dictionary[tword.downcase]
+    else tweet_words
     end
-    
+    new_tweet_words
   end
-  tweet_words.join(" ")
+  new_tweet_words.join(" ")
 end
 
 
